ed93a486f13d1f22f429d00135531500cdc3b03c
健丰 税 <win32@126.com>
2025-02-06 10:29:26 +0800

    添加

--- wwwroot/js/chat.js
+++ wwwroot/js/chat.js
@@ -666,13 +666,6 @@ class ChatUI {
         const messageDiv = document.createElement('div');
         messageDiv.className = `message ${role}-message`;
 
-        //// 创建头像和图标容器
-        //const avatarDiv = document.createElement('div');
-        //avatarDiv.className = 'message-avatar';
-
-        //// 根据角色选择不同的图标
-        //const iconSvg = this.getIconByRole(role);
-        //avatarDiv.innerHTML = iconSvg;
 
         const containerDiv = document.createElement('div');
         containerDiv.className = 'message-container';
@@ -697,11 +690,9 @@ class ChatUI {
         deleteButton.innerHTML = `
     <svg class="icon" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
         <path d="M5.5 5.5A.5.5 0 016 6v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm2.5 0a.5.5 0 01.5.5v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm3 .5a.5.5 0 00-1 0v6a.5.5 0 001 0V6z"/>
-        <path fill-rule="evenodd" d="M14.5 3a1 1 0 01-1 1H13v9a2 2 0 01-2 2H5a2 2 0 01-2-2V4h-.5a1 1 0 01-1-1V2a1 1 0 011-1H6a1 1 0 011-1h2a1 1 0 011 1h3.5a1 1 0 011 1v1zM4.118 4L4 4.059V13a1 1 0 001 1h6a1 1 0 001-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-    </svg>
-`;
+        <path fill-rule="evenodd" d="M14.5 3a1 1 0 01-1 1H13v9a2 2 0 01-2 2H5a2 2 0 01-2-2V4h-.5a1 1 0 01-1-1V2a1 1 0 011-1h3.5l1-1h4a1 1 0 011 1v1zM4.118 4L4 4.059V13a1 1 0 001 1h6a1 1 0 001-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+    </svg>`;
 
-        
         // 添加删除事件监听
         deleteButton.addEventListener('click', () => {
             this.deleteMessage(messageDiv);
@@ -770,7 +761,6 @@ class ChatUI {
     deleteMessage(messageElement) {
         if (confirm('确定要删除这条消息吗？')) {
             this.messagesContainer.removeChild(messageElement);
-            // 如果需要同步删除到服务器或进行其他操作，请在这里添加相应代码
         }
     }
 
